15e522aef82db1ca3c3d25d22f11f84f4962d2c0
awarberg <andreas@warberg.net>
2014-07-26 15:26:41 +0200

    Renamed solution to match its contents

---
+++ PerDevEnvSettingsDemo/Web/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+using System.Web.Mvc;
+using PerDevEnvSettingsDemo.Web.Models;
+
+namespace PerDevEnvSettingsDemo.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            var model = new IndexModel
+            {
+                MyConnectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString,
+                MyAppSettingsValue = ConfigurationManager.AppSettings["MyAppSettingsValue"]
+            };
+            return View(model);
+        }
+	}
+}
---
+++ PerDevEnvSettingsDemo/Web/Models/IndexModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PerDevEnvSettingsDemo.Web.Models
+{
+    public class IndexModel
+    {
+        public string MyConnectionString { get; set; }
+
+        public string MyAppSettingsValue { get; set; }
+    }
+}
