c6c310303c4bad7b8d1122983c28a85acb4f493f
awarberg <andreas@warberg.net>
2015-03-05 21:17:19 +0100

    Adjusted file name to match primary interface

---
+++ JsonConditionalSerialization/Web/Code/IConditionallySerialized.cs
@@ -0,0 +1,12 @@
+﻿namespace JsonConditionalSerialization.Code
+{
+    public interface IConditionallySerialized
+    {
+        ISerializedPropertyNameCollection SerializedPropertyNameCollection { get; }
+    }
+
+    public interface ISerializedPropertyNameCollection
+    {
+        bool Contains(string propertyName);
+    }
+}
